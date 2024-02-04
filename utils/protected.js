ce7a8ee113b3f52931f7d065d6611858ce13182d
ramlow07 <luamramlow@gmail.com>
2024-02-03 22:10:14 -0300

    protected routes: adding a middleware to verify the access token and check if the user exists

---
+++ utils/protected.js
@@ -0,0 +1,47 @@
+const { verify } = require("jsonwebtoken");
+const User = require("../models/user");
+const protected = async (req, res, next) => {
+    // get the token from the header
+    const authorization = req.headers["authorization"];
+    // if we don't have a token, return error
+    if (!authorization)
+    return res.status(500).json({
+        message: "Invalid token",
+        type: error,
+    });
+
+    // if we have a token, you have to verify it
+    const token = authorization.split("")[1];
+let id;
+try {
+    id = verify(token, process.env.ACCES_TOKEN_SECRET).id;
+} catch {
+    return res.status(500).json({
+        message: "Invalid token",
+        type: error,
+    });
+}
+
+// if the token is invalid, return error
+
+if (!id)
+return res.status(500).json({
+    message: "Invalid token",
+    type: error,
+});
+
+// if the token is valid, check if the user exists
+const user = await User.findById(id);
+// if the user doesn't exists, return error
+if (!user)
+return res.status(500).json({
+    message: "User doesn't exists",
+    type: error,
+});
+
+// if the user exists, we'll add a new field "user" to the request
+req.user = user;
+
+// call the next middleware
+next();
+};
